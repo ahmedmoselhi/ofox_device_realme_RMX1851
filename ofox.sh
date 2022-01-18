0b3c61f986b737463a9e344341188d1225123e55
Ahmed moselhi <ahmedmoselhi55@gmail.com>
2022-01-18 21:52:48 +0530

    update ofox.sh . Add upload to hyper

--- ofox.sh
+++ ofox.sh
@@ -1,56 +1,111 @@
 #!/bin/bash
-TARGET_DEVICE=RMX1851
-# configure some default settings for the build
-Default_Settings() {
-#export FOX_USE_LZMA_COMPRESSION="1"
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_ADVANCED_SECURITY="1"
-export FOX_ADVANCED_STOCK_REPLACE="1"
-export FOX_BUILD_FULL_KERNEL_SOURCES="1"
-export FOX_BUILD_TYPE="Stable"
-export FOX_DELETE_AROMAFM="1"
-export FOX_R11="1"
-export FOX_REPLACE_BUSYBOX_PS="1"
-export FOX_RESET_SETTINGS="1"	
-export FOX_USE_BASH_SHELL="1"
-export FOX_USE_NANO_EDITOR="1"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-export FOX_VERSION="R11.0_1"
-export TW_DEVICE_VERSION="R11.0_1"
-export LC_ALL="C"
-export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
-export OF_FLASHLIGHT_ENABLE="1"
-export OF_MAINTAINER="AJITH"
-export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-#export OF_PATCH_AVB20="1"
-export OF_SCREEN_H="2340"
-export OF_STATUS_INDENT_LEFT="48"
-export OF_STATUS_INDENT_RIGHT="48"
-export OF_SUPPORT_OZIP_DECRYPTION="1"
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-export OF_USE_NEW_MAGISKBOOT="1"
-export TARGET_ARCH="arm64"
-export USE_CCACHE="1"
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="RMX1851"
 
-	# lzma
-  	[ "$FOX_USE_LZMA_COMPRESSION" = "1" ] && export LZMA_RAMDISK_TARGETS="recovery"
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+    if [ -n "$chkdev" ]; then
+        FOX_BUILD_DEVICE="$FDEVICE"
+    else
+        chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+    fi
 }
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+    fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    export OF_MAINTAINER="kanged99"
+    export FOX_VERSION="R11.1_2"
+    export FOX_BUILD_TYPE="Stable"
+    export TARGET_ARCH=arm64
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export OF_STATUS_INDENT_LEFT=48
+    export OF_STATUS_INDENT_RIGHT=48
+    export OF_HIDE_NOTCH=1
+    export OF_ALLOW_DISABLE_NAVBAR=0
+    export OF_CHECK_OVERWRITE_ATTEMPTS=1
+    export OF_RUN_POST_FORMAT_PROCESS=1
+    export FOX_ADVANCED_SECURITY=1
+    export OF_NO_SAMSUNG_SPECIAL=1
+    export OF_USE_TWRP_SAR_DETECT=1
+    export FOX_DELETE_AROMAFM=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+    export OF_SKIP_ORANGEFOX_PROCESS=1
+    export OF_SUPPORT_OZIP_DECRYPTION=1
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
+    export FOX_USE_SPECIFIC_MAGISK_ZIP="/home/hyper/ahmed/ofox/device/realme/RMX1851/Magisk_Canary.zip"
+    export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+    export OF_TWRP_COMPATIBILITY_MODE=1
+    export FOX_REPLACE_BUSYBOX_PS=1
+    export FOX_REPLACE_TOOLBOX_GETPROP=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_SED_BINARY=1
+    export FOX_USE_LZMA_COMPRESSION=1
+    export OF_ENABLE_LPTOOLS=1
+    export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+    export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+    # lzma
+    [ "$FOX_USE_LZMA_COMPRESSION" = "1" ] && export LZMA_RAMDISK_TARGETS="recovery"
+	
+    # Let's see which are our build vars
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
+fi
 
 # build the project
 do_build() {
-  Default_Settings
 
   # use ccache ??
   [ "$USE_CCACHE" = "1" ] && ccache -M 20G
 
   # compile it
+  cd ~/ahmed/ofox/
+  export ALLOW_MISSING_DEPENDENCIES=true
   . build/envsetup.sh
 
-  lunch omni_"$TARGET_DEVICE"-eng
+  lunch omni_"$FOX_BUILD_DEVICE"-eng
 
   time mka recoveryimage && cd device/realme/RMX1851
 }
 
 # --- main --- #
 cd .. && cd .. && cd .. && do_build
-#
+
+# --- upload --- #
+zipfile=$(find /nvme/home/hyper/ahmed/ofox/out/target/product/RMX1851/ -iname OrangeFox-*.zip -printf "%f\n")
+scp /nvme/home/hyper/ahmed/ofox/out/target/product/RMX1851/$zipfile hyper@hyper.remainsilent.net:/var/www/html/builds/roms/RMX1851
+echo " file uploaded successfully . please download at http://hyper.remainsilent.net/builds/roms/RMX1851/$zipfile "
